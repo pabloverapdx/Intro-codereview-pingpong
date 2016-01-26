8b3b61cff1822a7a4a4ac92968473256dc0b075b
Pablo Vera <pablovera.pdx@gmail.com>
2016-01-25 17:47:02 -0800

    add first passing test

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,34 @@
-var stoppingPoint = prompt("type any positive interger")
+var checkNumber = function (stoppingPoint){
+if (stoppingPoint !== NaN) {
+    return true;
+}
 
-for (i = 1; i++; i <= stoppingPoint) {
-  if (i % 15 === 0) {
-    document.write("pingpong");
-  }
-  else if (i % 3 === 0) {
-    document.write("ping");
-  }
-  else if (i % 5 === 0) {
-    document.write("pong");
-  }
-  else {
-    document.write("i");
-  }
+};
 
-})
+
+
+
+//Business Logic //
+
+// var stoppingPoint = prompt("type any positive interger")
+// var stoppingPoint = function (stoppingPoint) {
+//
+// for (i = 1; i++; i <= stoppingPoint) {
+//   if (i % 15 === 0) {
+//     document.write("pingpong");
+//   }
+//   else if (i % 3 === 0) {
+//     document.write("ping");
+//   }
+//   else if (i % 5 === 0) {
+//     document.write("pong");
+//   }
+//   else {
+//     document.write("i");
+//   }
+//
+// });
+// }var stoppingPoint = function ()
+// //Logic //
+// (document).ready(function() {
+//   $("form#pingPong").submit(function(event) {
--- spec/specs.js
+++ spec/specs.js
@@ -1,14 +1,14 @@
 describe ('stoppingPoint', function() {
-  it ("will list a numbers from 1 to enterNumber", function () {
-    expect(stoppingPoint)(2).to.eql([1, 2]);
+  it ("is true if input is a number", function () {
+    expect(checkNumber(1)).to.equal(true);
 });
- it ("will say 'ping' if the number is divisible by 3", function () {
-   expect(stoppingPoint)(3).to.eql([1, 2, "ping"]);
-});
-  it ("will say 'pong' if the number is divisible by 5", function () {
-    expect(stoppingPoint)(5).to.eql([1, 2, 3, 4, "pong"]);
-});
-  it ("will say 'pingpong' if the number is divisible by 3 and 5", function () {
-    expect(stoppingPoint)(15).to.eql([1, 2, 'ping', 4, 'pong', 'ping', 7, 8, 'ping', 'pong', 11, 'ping', 13, 14, 'pingpong'])
-});
+//  it ("will say 'ping' if the number is divisible by 3", function () {
+//    expect(stoppingPoint)(3).to.eql([1, 2, "ping"]);
+// });
+//   it ("will say 'pong' if the number is divisible by 5", function () {
+//     expect(stoppingPoint)(5).to.eql([1, 2, 3, 4, "pong"]);
+// });
+//   it ("will say 'pingpong' if the number is divisible by 3 and 5", function () {
+//     expect(stoppingPoint)(15).to.eql([1, 2, 'ping', 4, 'pong', 'ping', 7, 8, 'ping', 'pong', 11, 'ping', 13, 14, 'pingpong'])
+//   });
 });
