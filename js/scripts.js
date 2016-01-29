af8bf4f955d784e7f9c1b2e17d66440900833503
Pablo Vera <pablovera.pdx@gmail.com>
2016-01-28 23:18:16 -0800

    third test of numbers divisible by 5

--- js/scripts.js
+++ js/scripts.js
@@ -2,4 +2,7 @@ var pingPong = function (number) {
   if (number % 3 === 0) {
    return "ping";
   }
+  else if (number % 5 === 0) {
+    return "pong";
+  }
 }
--- spec/specs.js
+++ spec/specs.js
@@ -2,4 +2,8 @@ describe('pingPong', function () {
   it("is a number divisible by 3", function () {
     expect(pingPong(6)).to.equal("ping");
   });
- });
+
+  it("is a number divisible by 5", function () {
+    expect(pingPong(5)).to.equal("pong");
+  });
+});
