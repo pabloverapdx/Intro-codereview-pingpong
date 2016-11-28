d8da89a42b6a96aa6bd836ccacd1fbbf1d723ca9
Pablo Vera <pablovera.pdx@gmail.com>
2016-11-28 00:05:40 -0800

    change layout and style with css - add alert when somebody enters zero o a negative number

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 var pingPong = function (number) {
   if (number % 15 === 0) {
-   return "pingpong";
+   return "ping - pong";
   }
   else if (number % 5 === 0) {
     return "pong";
@@ -16,9 +16,10 @@ var pingPong = function (number) {
 $(document).ready(function() {
   $("form#wantToPing").submit(function(event) {
     var number = $("input#number").val();
-        number = parseInt(number);
-
     $("#results").empty();
+    if (number <= 0){
+  alert("Please enter a number greater than or equal to 1!");
+}
     for (var i = 1; i <= number; i +=1) {
       var result = pingPong (i);
         $("<li>" + result + "</li>").appendTo($("#results"));
